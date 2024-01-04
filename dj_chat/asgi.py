d34b566b823d1c997874a77c4b8827a894e116e1
Mathefagbe <fagbemi65@gmail.com>
2024-01-04 10:59:07 +0100

    added staticfiles

--- dj_chat/asgi.py
+++ dj_chat/asgi.py
@@ -1,13 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
-from mychat.routing import websocket_urlpatterns
+
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
 
 django_asgi_app = get_asgi_application()
+
+from mychat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
  'http': django_asgi_app,
