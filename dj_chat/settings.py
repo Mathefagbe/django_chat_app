f77e65f9a148eab12902be276fc0894a195cf5c8
Mathefagbe <fagbemi65@gmail.com>
2024-01-06 13:25:37 +0100

    change login

--- dj_chat/settings.py
+++ dj_chat/settings.py
@@ -82,20 +82,20 @@ ASGI_APPLICATION = 'dj_chat.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 # else:
-# DATABASES = {
+DATABASES = {
 
-#             # You can remove to use mysql
-#             'default': dj_database_url.parse(config('DATABASE_URL'))
+            # You can remove to use mysql
+            'default': dj_database_url.parse(config('DATABASE_URL'))
         
-# }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
